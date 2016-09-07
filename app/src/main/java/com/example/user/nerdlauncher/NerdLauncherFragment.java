043d0349cd5b3e973abb537c51b1aa4bb5b0ec25
walkermannum1 <2469377884@qq.com>
2016-09-07 17:42:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,50 @@
+package com.example.user.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by user on 2016/9/7.
+ */
+public class NerdLauncherFragment extends Fragment {
+
+    private static final String TAG = "NerdLauncherFragment";
+
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        setupAdapter();
+
+        return v;
+    }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + "activities.");
+    }
+}
